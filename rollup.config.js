f19c91375a0cf5322bc857c295348c9aab2b6b0f
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2023-04-09 19:28:10 +0330

    feat: add rollup configs

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import packageJson from "./package.json" assert { type: "json" };
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import peerDependencies from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "./src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        type: "cjs",
+        sourcemap: true,
+        name: "react-context-toolkit",
+      },
+      {
+        file: packageJson.module,
+        type: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      terser(),
+      commonjs(),
+      postcss(),
+      resolve(),
+      peerDependencies(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [
+      {
+        file: "dist/index.d.ts",
+        format: "esm",
+      },
+    ],
+    external: [/\.css$/],
+    plugins: [dts()],
+  },
+];
